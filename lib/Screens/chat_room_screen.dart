2720c21a61e6cfd33db68fc8700390aafad8c217
Mubarik Nirban <mubariknirban786@gmail.com>
2023-02-14 19:16:44 +0530

    eleventest2

--- lib/Screens/chat_room_screen.dart
+++ lib/Screens/chat_room_screen.dart
@@ -102,10 +102,8 @@ class _ChatRoomState extends State<ChatRoom> {
                     .orderBy('time',descending: false).snapshots(),
                 builder: (BuildContext context,AsyncSnapshot<QuerySnapshot> snapshot){
                   if(snapshot.data != null){
-
                     return ListView.builder(
                          itemCount: snapshot.data!.docs.length,
-
                          itemBuilder: (context,index){
 
                            Map<String, dynamic>? map = snapshot.data!.docs[index].data() as Map<String, dynamic>?;
